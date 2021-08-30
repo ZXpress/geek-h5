47d7708757955fe8d65ee01ee8de5a0d44386742
kevinzhang <kevin.zhang@moredist.com>
2021-08-30 16:24:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -20,30 +20,25 @@ function App() {
 
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/home/index" />} />
-        <Route
-          exact
-          path="/home"
-          render={() => <Redirect to="/home/index" />}
-        />
+        <Route exact path="/home" render={() => <Redirect to="/home/index" />} ƒ />
 
         <Route path="/login" component={Login} />
         <Route path="/search" component={Search} />
         <Route path="/article/:id" component={Article} />
         <Route path="/search/result" component={SearchResult} />
+
         <AuthRoute path="/profile/edit" component={ProfileEdit} />
         <AuthRoute path="/profile/feedback" component={ProfileFeedback} />
         <AuthRoute path="/profile/chat" component={Chat} />
+
         {/* <Route path="*" component={NotFound} /> */}
         {/* 注意：因为 /home 不在 Switch 内部，所以，需要手动处理 /home 开头的路由，否则，会被当做 404 处理 */}
-        <Route
-          path="*"
-          render={props => {
-            if (props.location.pathname.startsWith('/home')) {
-              return null
-            }
-            return <NotFound {...props} />
-          }}
-        />
+        <Route path="*" render={props => {
+          if (props.location.pathname.startsWith('/home')) {
+            return null
+          }
+          return <NotFound {...props} />
+        }} />
       </Switch>
     </Router>
   )
